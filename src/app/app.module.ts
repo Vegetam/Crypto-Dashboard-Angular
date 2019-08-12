1a64a1df796a51ae16085c87a44ecd47cd4dc347
vegetam <francesco.malagrino@gmail.com>
2019-08-12 01:20:59 +0100

    App done
    App Done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { NgComponent } from './ng/ng.component';
-import { ListComponent } from './list/list.component';
-import { ListCardComponent } from './list-card/list-card.component';
-import { SearchFilterComponent } from './search-filter/search-filter.component';
+import {AppComponent} from './app.component';
+import {SearchFilterComponent} from './search-filter/search-filter.component';
+import {ListCardComponent} from './list-card/list-card.component';
+import {ListComponent} from './list/list.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {FormsModule} from '@angular/forms';
+import {MultiselectDropdownModule} from 'angular-2-dropdown-multiselect';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent,
-    NgComponent,
-    ListComponent,
+    SearchFilterComponent,
     ListCardComponent,
-    SearchFilterComponent
+    ListComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    MultiselectDropdownModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+/*
+import {Component} from '@angular/core';
+// import AppService
+import {AppService} from '../app.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
+  providers: [AppService]
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent {
+results: string[];
+data: string[][];
+coinValue: any;
+coinSymbol: any;
+API_BASE_URL = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest';
+ key = '?CMC_PRO_API_KEY=8ccfe4e8-7cf1-4a84-9b2f-f0047b8d3365';
+  // Inject HttpClient into your component or service.
+  constructor(private http: HttpClient) {}
+  ngOnInit(): void {
 
-  constructor() { }
+    // Make the HTTP request:
+     this.http
+    .get(this.API_BASE_URL + this.key)
+    .subscribe(data => {
+      // Read the result field from the JSON response.
+      this.results = data['results'];
+	  console.log(data);
+	  //console.log(JSON.stringify(data.data));
+	 /* let i;
+	  this.coinValue = [];
+	 this.coinSymbol = [];
+	for (i = 0; i < data.data.length; i++) { 
+			  this.coinValue += data.data[i].name;
+			  this.coinSymbol += data.data[i].symbol;
+				}
+			console.log(this.coinValue);
 
-  ngOnInit() {
-  }
-
+	});
 }
+}
+*/
+import {Component} from '@angular/core';
+// import AppService
+import {AppService} from '../app.service';
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css'],
+  providers: [AppService] // provider setup
+})
+export class DashboardComponent {}
